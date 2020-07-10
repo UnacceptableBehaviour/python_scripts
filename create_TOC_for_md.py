07aad5d6de5d90753742a921d8851f86e0936f9c
Simon <simon@ferdiesfoodlab.co.uk>
2020-07-10 21:50:12 +0100

    add comments arg to create_TOC.py

--- create_TOC_for_md.py
+++ create_TOC_for_md.py
@@ -1,19 +1,39 @@
 #! /usr/bin/env python
 # 3.7
 # create MD for TOC
+# set DEFAULT_FILE to relevant RTF course notes
+# DEFAULT_README = Path('./README.md')        # < in not using equations
+# DEFAULT_README = Path('./README.tex.md')    # < if Texify installed it will convert the Latex into equations
 
+# passing a .md file as argument will print a TOC for that file
 
 import os
+import subprocess
 import re
 from pprint import pprint
 import sys                          # argv
 #from pprint import  pprint
 
+from pathlib import Path
+
+# RTF conversion to text
+from striprtf.striprtf import rtf_to_text
+
+# convert RTF to txt
+def get_text_content_of_file(rtf_filepath):
+    
+    with open(rtf_filepath,'r') as f:
+        rtf = f.read()
+            
+    return rtf_to_text(rtf)             # convert to text and return
+
+
 def create_toc_link_text(title):
     
     # downcase, remove all non alphanumeric, replace space with hyphen
+    # leave hyphens in!
     toc_link_text = title.strip().strip("\\").lower()
-    toc_link_text = re.sub(r'[^a-z 0-9]', '', toc_link_text)
+    toc_link_text = re.sub(r'[^a-z 0-9\-]', '', toc_link_text)
     toc_link_text = re.sub(r' ', '-', toc_link_text)
     
     return toc_link_text
@@ -85,15 +105,38 @@ def create_TOC_from_text(text):
        
     return indented_md_hrefs 
     
+# Look at how to do this with functools.reduce()
+# https://stackoverflow.com/questions/952914/how-to-make-a-flat-list-out-of-list-of-lists
+#
+def create_TOC_as_string_from_TOC_nested_list(toc_list):
+    lines = ""
+    
+    for l in toc_list:
+        if type(l).__name__ == 'str':
+            lines += f"{l}  \n"
+        elif type(l).__name__ == 'list':
+            lines += create_TOC_as_string_from_TOC_nested_list(l)
 
+    return lines
 
-
-DEFAULT_FILE = 'context.md'
-
+# INPUT
+#DEFAULT_FILE = Path('/_COURSES_00_WIP/ALGO_00_Intro_2_Algorithms_MIT.rtf')
+#DEFAULT_FILE = Path('context.md')
+DEFAULT_FILE = Path('README.md')
 def get_mark_down(filename=DEFAULT_FILE):
         
     with open(filename) as f:
         content = f.read()
+
+    # incase string passed in
+    if Path(filename).suffix == '.rtf':
+        content = rtf_to_text(content)
+    else:
+        print(f"> > > > - - - - - - - - - - < < < < {Path(filename).suffix} > > > >")
+
+    replacement = ''
+    # remove anything inside 'comment' delimiters //* this is a comment *//
+    content =  re.sub(r'\/\/\*.*?\*\/\/', replacement, content, flags = re.MULTILINE | re.DOTALL)
 
     for line in iter(content.splitlines()):
         print(line)
@@ -101,6 +144,23 @@ def get_mark_down(filename=DEFAULT_FILE):
     print("\n\n\n\n\n\n")
     
     return content        
+    
+# OUTPUT - w/ TOC added
+DEFAULT_README = Path('./README.md')
+#DEFAULT_README = Path('./README.tex.md')    # if Texify installed it will convert the Latex into equations
+def save_text_w_toc_to_readme(text, toc_string):
+    
+    replacement = "## Contents  \n" + toc_string + "\n\n## AIM:  \n"
+    
+    print(f"***\n***\n{replacement}\n***\n***\n")
+    
+    text = re.sub(r'^## Contents.*?## AIM:.*?$', replacement, text, flags = re.MULTILINE | re.DOTALL)
+
+    with open(DEFAULT_README, 'w') as f:
+        f.write(text)
+
+    return text
+    
     
     
 if __name__ == '__main__':
@@ -110,15 +170,21 @@ if __name__ == '__main__':
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     #
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    
+    # title = 'Logarithms - identities & basic manipulation'
+    # link_sb = 'logarithms---identities--basic-manipulation'
+    # link = create_toc_link_text(title) 
+    # print(link_sb)
+    # print(link)
+    # print(link == link_sb)
+    # 
+    # sys.exit(0)
 
     report = f"PWD: {os.getcwd()}"
     
     # sys.argv[0] is name of this file
-    # prefer
-    # import pathlib; Path(sys.argv[1]).exists()   # but since we've already imported os
-    
 
-    if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):  
+    if len(sys.argv) > 1 and Path(sys.argv[1]).exists():  
         report += f"\nCreating TOC for {sys.argv[1]}"
         text = get_mark_down(sys.argv[1])
             
@@ -126,7 +192,32 @@ if __name__ == '__main__':
         report += f"\n* * USING DEFAULT FILE * * - Creating TOC for {DEFAULT_FILE}"
         text = get_mark_down()
     
-    print("- - - - - - - - - -")
-    create_TOC_from_text(text)
+
+    toc = create_TOC_from_text(text)
+
+    toc_string = create_TOC_as_string_from_TOC_nested_list(toc)
+
+    print(save_text_w_toc_to_readme(text, toc_string))
+        
+    return_code = 'WARNING: No push to git!'
+    commit_comment = 'No comment'
+    if '-p' in sys.argv:
+        try:
+            commit_comment = sys.argv[sys.argv.index('-p')+1]
+        except IndexError:
+            commit_comment = input("Commit comment:")
+        
+        return_code = subprocess.call(['git','add',DEFAULT_README]) # note command separation inside list! [ ]
+        print(f"git add {DEFAULT_README}: {return_code}")          # ^----/
+            
+        return_code = subprocess.call(["git","commit",f"-m autogen {DEFAULT_README}:{commit_comment}"])
+        print(f"git commit -m'autogen {DEFAULT_README}: {commit_comment}'")
+    
+        return_code = subprocess.call(['git','push'])
+        print(f"git push: {return_code}")        
+    else:
+        print(f"\n\n{return_code}\n")
+    
+    print(f"\n\n{toc_string}\n\n")
     
     print(f"\n\n{report}")
